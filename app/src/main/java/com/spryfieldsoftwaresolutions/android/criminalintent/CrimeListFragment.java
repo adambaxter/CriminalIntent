5408270142b74bc2bf4d9d4352aec7fbf7051a44
Adam Baxter <adambaxter55@gmail.com>
2018-02-02 23:23:35 -0400

    Empty fragment to be updated later.

---
+++ app/src/main/java/com/spryfieldsoftwaresolutions/android/criminalintent/CrimeListFragment.java
@@ -0,0 +1,10 @@
+package com.spryfieldsoftwaresolutions.android.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by slim on 02/02/18.
+ */
+
+public class CrimeListFragment extends Fragment {
+}
