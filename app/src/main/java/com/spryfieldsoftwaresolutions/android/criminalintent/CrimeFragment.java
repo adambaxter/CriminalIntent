8f3b092843ca5c88aaea2211609ebe06685fd81f
Adam Baxter <adambaxter55@gmail.com>
2018-02-06 12:12:21 -0400

    - Added ability to pull the UUID of a crime out of the Intent. Now we can get the crime that owns the UUID out of the crimeLab

--- app/src/main/java/com/spryfieldsoftwaresolutions/android/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/spryfieldsoftwaresolutions/android/criminalintent/CrimeFragment.java
@@ -42,6 +42,7 @@ public class CrimeFragment extends Fragment{
         View v = inflater.inflate(R.layout.fragment_crime, container, false);
 
         mTitleField = v.findViewById(R.id.crime_title);
+        mTitleField.setText(mCrime.getTitle());
         mTitleField.addTextChangedListener(new TextWatcher(){
             @Override
             public void beforeTextChanged(
@@ -67,6 +68,7 @@ public class CrimeFragment extends Fragment{
         mDateButton.setEnabled(false);
 
         mSolvedCheckBox = v.findViewById(R.id.crime_solved);
+        mSolvedCheckBox.setChecked(mCrime.isSolved());
         mSolvedCheckBox.setOnCheckedChangeListener(new OnCheckedChangeListener(){
             @Override
             public void onCheckedChanged(CompoundButton buttonView,
