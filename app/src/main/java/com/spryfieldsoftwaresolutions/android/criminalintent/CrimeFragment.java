f4df3c4f857d19777e66bc1e0f803e00d5126558
Adam Baxter <adambaxter55@gmail.com>
2018-02-06 12:29:59 -0400

    - Added fragment args to hold the crime UUID

--- app/src/main/java/com/spryfieldsoftwaresolutions/android/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/spryfieldsoftwaresolutions/android/criminalintent/CrimeFragment.java
@@ -27,6 +27,18 @@ public class CrimeFragment extends Fragment{
     private Button mDateButton;
     private CheckBox mSolvedCheckBox;
 
+    private static final String ARG_CRIME_ID = "crime_id";
+
+    public static CrimeFragment newInstance(UUID crimeId) {
+        Bundle args = new Bundle();
+        args.putSerializable(ARG_CRIME_ID, crimeId);
+
+        CrimeFragment fragment = new CrimeFragment();
+        fragment.setArguments(args);
+
+        return fragment;
+    }
+
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
