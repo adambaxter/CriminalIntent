86f16dd09b1173e0dd688751d613bef096769bcd
Adam Baxter <adambaxter55@gmail.com>
2018-02-08 00:10:59 -0400

    Added the clock dialog to TimePickerFragment.

--- app/src/main/java/com/spryfieldsoftwaresolutions/android/criminalintent/TimePickerFragment.java
+++ app/src/main/java/com/spryfieldsoftwaresolutions/android/criminalintent/TimePickerFragment.java
@@ -4,6 +4,8 @@ import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.view.View;
 
 /**
  * Created by slim on 07/02/18.
@@ -14,7 +16,11 @@ import android.support.v7.app.AlertDialog;
 public class TimePickerFragment extends DialogFragment {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+        View v = LayoutInflater.from(getActivity())
+                .inflate(R.layout.dialog_time, null);
+
         return new AlertDialog.Builder(getActivity())
+                .setView(v)
                 .setTitle(R.string.time_picker_title)
                 .setPositiveButton(android.R.string.ok, null)
                 .create();
