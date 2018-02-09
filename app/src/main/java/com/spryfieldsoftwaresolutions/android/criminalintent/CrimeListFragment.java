f60170fe1f654b7d0eaa6ee2fc9c5137223beeac
Adam Baxter <adambaxter55@gmail.com>
2018-02-08 22:40:42 -0400

    Fixed UI not updating CrimesList when adding a new crime.

--- app/src/main/java/com/spryfieldsoftwaresolutions/android/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/spryfieldsoftwaresolutions/android/criminalintent/CrimeListFragment.java
@@ -10,6 +10,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -57,6 +58,21 @@ public class CrimeListFragment extends Fragment {
         inflater.inflate(R.menu.fragment_crime_list, menu);
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.new_crime:
+                Crime crime = new Crime();
+                CrimeLab.get(getActivity()).addCrime(crime);
+                Intent intent = CrimePagerActivity
+                        .newIntent(getActivity(), crime.getId());
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
     private void updateUI() {
         CrimeLab crimeLab = CrimeLab.get(getActivity());
         List<Crime> crimes = crimeLab.getCrimes();
@@ -69,6 +85,7 @@ public class CrimeListFragment extends Fragment {
             //    Log.d("CRIME_LIST_FRAGMENT", "Updated position: " + mLastPositionUpdated);
             //    mLastPositionUpdated = -1;
             // } else {
+            mAdapter.replaceList(crimes);
                 mAdapter.notifyDataSetChanged();
             //}
         }
@@ -184,7 +201,9 @@ public class CrimeListFragment extends Fragment {
             return LIST_ITEM_CRIME;
         }
 
-
+        public void replaceList(List<Crime> crimes) {
+            mCrimes = crimes;
+        }
     }
 
 }
