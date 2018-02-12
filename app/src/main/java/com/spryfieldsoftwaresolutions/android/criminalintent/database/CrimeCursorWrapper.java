77d289b9fbb73260b4e45748bb94d0a8777f6130
Adam Baxter <adambaxter55@gmail.com>
2018-02-12 01:27:46 -0400

    Changed date to a long instead of a string.

--- app/src/main/java/com/spryfieldsoftwaresolutions/android/criminalintent/database/CrimeCursorWrapper.java
+++ app/src/main/java/com/spryfieldsoftwaresolutions/android/criminalintent/database/CrimeCursorWrapper.java
@@ -6,6 +6,9 @@ import android.database.CursorWrapper;
 import com.spryfieldsoftwaresolutions.android.criminalintent.Crime;
 import com.spryfieldsoftwaresolutions.android.criminalintent.database.CrimeDbSchema.CrimeTable;
 
+import java.util.Date;
+import java.util.UUID;
+
 /**
  * Created by slim on 12/02/18.
  */
@@ -18,9 +21,13 @@ public class CrimeCursorWrapper extends CursorWrapper {
     public Crime getCrime() {
         String uuidString = getString(getColumnIndex(CrimeTable.Cols.UUID));
         String title = getString(getColumnIndex(CrimeTable.Cols.TITLE));
-        String date = getString(getColumnIndex(CrimeTable.Cols.DATE));
+        long date = getLong(getColumnIndex(CrimeTable.Cols.DATE));
         String time = getString(getColumnIndex(CrimeTable.Cols.TIME));
         int isSolved = getInt(getColumnIndex(CrimeTable.Cols.SOLVED));
+
+        Crime crime = new Crime(UUID.fromString(uuidString));
+        crime.setTitle(title);
+        crime.setDate(new Date(date));
 
         return null;
 
