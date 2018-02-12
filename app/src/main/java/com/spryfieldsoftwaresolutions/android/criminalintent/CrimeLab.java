cb2bb68380cc6ee85fc0961aff238b8c49b3bc98
Adam Baxter <adambaxter55@gmail.com>
2018-02-11 22:09:56 -0400

    Added code to create a database to store all crimes.

--- app/src/main/java/com/spryfieldsoftwaresolutions/android/criminalintent/CrimeLab.java
+++ app/src/main/java/com/spryfieldsoftwaresolutions/android/criminalintent/CrimeLab.java
@@ -1,6 +1,9 @@
 package com.spryfieldsoftwaresolutions.android.criminalintent;
 
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.spryfieldsoftwaresolutions.android.criminalintent.database.CrimeBaseHelper;
 
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
@@ -16,6 +19,8 @@ public class CrimeLab {
     private static CrimeLab sCrimeLab;
 
     private Map<UUID, Crime> mCrimes;
+    private Context mContext;
+    private SQLiteDatabase mDatabase;
 
     public static CrimeLab get(Context context){
         if(sCrimeLab == null){
@@ -25,6 +30,9 @@ public class CrimeLab {
     }
 
     private CrimeLab(Context context){
+        mContext = context.getApplicationContext();
+        mDatabase = new CrimeBaseHelper(mContext)
+                .getWritableDatabase();
         mCrimes = new LinkedHashMap<>();
     }
 
