f092afdb8e86c2ed8f8151a00fa400d4581b8f25
Adam Baxter <adambaxter55@gmail.com>
2018-02-11 21:59:08 -0400

    Crime database Schema

---
+++ app/src/main/java/com/spryfieldsoftwaresolutions/android/criminalintent/database/CrimeDbSchema.java
@@ -0,0 +1,20 @@
+package com.spryfieldsoftwaresolutions.android.criminalintent.database;
+
+/**
+ * Created by slim on 11/02/18.
+ * Database Schema
+ */
+
+public class CrimeDbSchema {
+    public static final class CrimeTable {
+        public static final String NAME = "crimes";
+
+        public static final class Cols {
+            public static final String UUID = "uuid";
+            public static final String TITLE = "title";
+            public static final String DATE = "date";
+            public static final String TIME = "time";
+            public static final String SOLVED = "solved";
+        }
+    }
+}
